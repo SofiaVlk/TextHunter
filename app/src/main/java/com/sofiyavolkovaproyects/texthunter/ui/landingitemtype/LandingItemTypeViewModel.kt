b6887507f11f844f6025764321abd7998cc820ab
sofiy <sofiya.volkova@gmail.com>
2023-10-01 15:56:59 +0200

    Initial commit

---
+++ app/src/main/java/com/sofiyavolkovaproyects/texthunter/ui/landingitemtype/LandingItemTypeViewModel.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sofiyavolkovaproyects.texthunter.ui.landingitemtype
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sofiyavolkovaproyects.texthunter.data.LandingItemTypeRepository
+import com.sofiyavolkovaproyects.texthunter.ui.landingitemtype.LandingItemTypeUiState.Error
+import com.sofiyavolkovaproyects.texthunter.ui.landingitemtype.LandingItemTypeUiState.Loading
+import com.sofiyavolkovaproyects.texthunter.ui.landingitemtype.LandingItemTypeUiState.Success
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LandingItemTypeViewModel @Inject constructor(
+    private val landingItemTypeRepository: LandingItemTypeRepository
+) : ViewModel() {
+
+    val uiState: StateFlow<LandingItemTypeUiState> = landingItemTypeRepository
+        .landingItemTypes.map<List<String>, LandingItemTypeUiState>(::Success)
+        .catch { emit(Error(it)) }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), Loading)
+
+    fun addLandingItemType(name: String) {
+        viewModelScope.launch {
+            landingItemTypeRepository.add(name)
+        }
+    }
+}
+
+sealed interface LandingItemTypeUiState {
+    object Loading : LandingItemTypeUiState
+    data class Error(val throwable: Throwable) : LandingItemTypeUiState
+    data class Success(val data: List<String>) : LandingItemTypeUiState
+}
